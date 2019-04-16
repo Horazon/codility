77a11b9df4774aa3a6214100c51712cd1b6f1b6e
horazon <horazon666@gmail.com>
2019-04-16 21:32:24 +0200

    solutions for lesson 005

---
+++ src/test/java/pl/horazon/codility/lessons/lesson005/GenomicRangeQueryTest.java
@@ -0,0 +1,11 @@
+package pl.horazon.codility.lessons.lesson005;
+
+import org.junit.jupiter.api.Test;
+
+class GenomicRangeQueryTest {
+
+    @Test
+    void solution() {
+        new GenomicRangeQuery().solution("CAGCCTA", new int[]{2, 5, 0}, new int[]{4, 5, 6});
+    }
+}
